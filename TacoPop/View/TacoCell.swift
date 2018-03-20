733c8329f4a3509a13475dcf8a216a2dfb37caf5
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-20 02:18:01 +0100

    Added Taco cell

---
+++ TacoPop/View/TacoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TacoCell.swift
+//  TacoPop
+//
+//  Created by Aleksei Degtiarev on 20/03/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    
+    var taco: Taco!
+    
+    func configureCell(taco: Taco){
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinId.rawValue)
+        tacoLabel.text = taco.productName
+    }
+    
+}
