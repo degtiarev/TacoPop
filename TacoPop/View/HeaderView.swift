558eae31328e59834d2848292cd2e22759399efb
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-20 01:02:27 +0100

    Added DropShadow to view

---
+++ TacoPop/View/HeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  HeaderView.swift
+//  TacoPop
+//
+//  Created by Aleksei Degtiarev on 20/03/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView, DropShadow {}
